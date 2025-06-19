df8cb2c4e184284fccbb197b2e36e436452408bc
Kev <kevinn858@gmail.com>
2025-06-19 16:11:26 -0700

    added profile picture and profile text

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 import ProfileCard from "./profile-card";
 
+const variables = {
+  name: "Sarah Dole",
+  title: "Front End Engineer @ Microsoft",
+  bio: "I turn coffee into bugs which are fixed by someone else. Certified Stack Overflow and ChatGPT developer.",
+  thumbnailSrc: "/profile-card/profile.png",
+};
+
 export default function Home() {
+  const { name, title, bio, thumbnailSrc } = variables;
+
   return (
     <div className="page flex flex-col items-center bg-gradient-to-b from-gray-50 to-[#d2d6db] py-[200px] h-full min-h-screen">
-      <ProfileCard />
+      <ProfileCard
+        name={name}
+        title={title}
+        bio={bio}
+        thumbnailSrc={thumbnailSrc}
+      />
 
       <footer>
         <div className="credits" data-gfe-screenshot-exclude="true">
--- app/profile-card.tsx
+++ app/profile-card.tsx
@@ -1,5 +1,43 @@
-export default function ProfileCard() {
-    return (
-        <h1>Profile Card</h1>
-    )
+import Image from "next/image";
+
+export interface ProfileCardProps {
+  thumbnailSrc: string;
+  name: string;
+  title: string;
+  bio: string;
+  alt?: string;
+}
+
+export default function ProfileCard({
+  thumbnailSrc,
+  name,
+  title,
+  bio,
+  alt = "profile picture",
+}: ProfileCardProps) {
+  return (
+    <div className="profile-card w-[340px] flex flex-col items-center gap-10 bg-white px-4 py-6 rounded-lg">
+      <div className="details flex flex-col items-center gap-6 self-stretch">
+        <Image
+          className="thumbnail"
+          src={thumbnailSrc}
+          alt={alt}
+          height={64}
+          width={64}
+        />
+        <div className="name-title flex flex-col justify-center items-center gap-1 self-stretch">
+          <h2 className="name font-medium text-xl text-center text-neutral-900">
+            {name}
+          </h2>
+          <h3 className="title font-normal text-sm text-center text-neutral-600">
+            {title}
+          </h3>
+        </div>
+        <p className="bio font-normal text-base text-center text-neutral-600">
+          {bio}
+        </p>
+      </div>
+      <div className="actions"></div>
+    </div>
+  );
 }
